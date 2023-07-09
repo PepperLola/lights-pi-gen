c89eb0b4745f48b4ad7f001d5a8b8b813fdaab62
PepperLola <julian.orion.wright@gmail.com>
2023-07-08 17:52:28 -0700

    handle when build run with residual lights files/folders

--- stage2/04-install-lights/00-run.sh
+++ stage2/04-install-lights/00-run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -e
+if [ -d "lights" ]; then
+        rm -r lights
+fi
 git clone https://github.com/PepperLola/lights.git
+if [ -d "${ROOTFS_DIR}/etc/piled" ]; then
+        rm -rf "${ROOTFS_DIR}/etc/piled"
+fi
 mkdir "${ROOTFS_DIR}/etc/piled"
-mv -r ./lights/pi/* "${ROOTFS_DIR}/etc/piled"
+mv ./lights/pi/* "${ROOTFS_DIR}/etc/piled"
 rm -rf ./lights
-python -m pip install -r "${ROOTFS_DIR}/etc/piled/requirements.txt" --target "${ROOTFS_DIR}/lib/python3/dist-packages"
+python3 -m pip install -r "${ROOTFS_DIR}/etc/piled/requirements.txt" --target "${ROOTFS_DIR}/lib/python3/dist-packages"
--- stage2/prerun.sh
+++ stage2/prerun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+apt-get -y install python3-pip
+
 if [ ! -d "${ROOTFS_DIR}" ]; then
 	copy_previous
 fi
