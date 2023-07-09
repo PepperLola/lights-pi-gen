005830872fe7eb2f9d9bff9753816d06e0a14928
PepperLola <julian.orion.wright@gmail.com>
2023-07-08 17:12:40 -0700

    install pip packages to right folder, make 00-run.sh executable

--- stage2/04-install-lights/00-run.sh
+++ stage2/04-install-lights/00-run.sh
@@ -3,3 +3,4 @@ git clone https://github.com/PepperLola/lights.git
 mkdir "${ROOTFS_DIR}/etc/piled"
 mv -r ./lights/pi/* "${ROOTFS_DIR}/etc/piled"
 rm -rf ./lights
+python -m pip install -r "${ROOTFS_DIR}/etc/piled/requirements.txt" --target "${ROOTFS_DIR}/lib/python3/dist-packages"
